00921dba9fffcfefdc4d54411eb697ac7385a310
Dylan <dylanbradley1@hotmail.com>
2022-10-17 14:42:43 +0300

    Added not found and error handler middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,28 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const { notFound, errorHandler } = require('./middlewares');
+
 const app = express();
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+  cors({
+    // Only requests coming from here can reach the backend
+    origin: 'http://localhost:3000',
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World, from index.js!',
+  });
+});
+
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 1337;
 
