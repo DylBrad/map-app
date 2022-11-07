65c6489e1162463c3e5134571e7bbd34ac302da3
Dylan <dylanbradley1@hotmail.com>
2022-11-07 16:21:28 +0100

    Update import links

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import * as React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import MapDisplay from "./components/MapDisplay";
-import OnBoarding from "./components/OnBoarding/OnBoarding";
-import Profile from "./components/Profile/Profile";
+import MapDisplay from './pages/MapDisplay/MapDisplay';
+import OnBoarding from './pages/OnBoarding/OnBoarding';
+import Profile from './pages/Profile/Profile';
 
 const App = () => {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MapDisplay />} />
-        <Route path="/onboarding" element={<OnBoarding />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MapDisplay />} />
+          <Route path="/onboarding" element={<OnBoarding />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
-}; 
+};
 
 export default App;
