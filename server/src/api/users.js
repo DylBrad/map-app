a786c7acf78dbb37a5f77a7b3f2c3b480f0d2ebc
Dylan <dylanbradley1@hotmail.com>
2022-11-09 18:02:59 +0100

    Sanitise emails and check if email already exists

--- server/src/api/users.js
+++ server/src/api/users.js
@@ -21,6 +21,15 @@ router.post('/', async (req, res, next) => {
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
 
+    const sanitisedEmail = user.email.toLowerCase();
+
+    const existingUser = await User.findOne({ email: sanitisedEmail });
+    if (existingUser) {
+      return res.status(409).send('This email is already in use.');
+    }
+
+    user.email = sanitisedEmail;
+
     const createdUser = await user.save();
     res.json(createdUser);
   } catch (error) {
@@ -29,6 +38,7 @@ router.post('/', async (req, res, next) => {
     }
     next(error);
   }
+  return 'success';
 });
 
 module.exports = router;
